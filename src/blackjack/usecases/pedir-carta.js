24c61961b9bac08b23a5e6fcbbb9345c6eb53460
Daniel Bautista <danibautista410@gmail.com>
2023-12-08 01:34:18 +0100

    First commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,15 @@
+
+/**
+ * Pedir una carta
+ * @param {Array<String>} deck es un arreglo de string
+ * @returns {String} retorna una carta
+ */
+
+export const pedirCarta = (deck) => {
+
+
+    if ( !deck || deck.length === 0 ) throw new Error('No hay cartas en el deck');
+    
+    const carta = deck.pop();
+    return carta;
+}
